9deb3b56f4f319000988897e589eb5cdc4bd4e40
Tiago Patricio <tiagoudp@gmail.com>
2020-04-05 16:02:51 +0100

    Updates

---
+++ precache-manifest.94cee93c87239c69f2e44cb74ed1fc3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79d2b7c4748397f6321e73803aebdfed",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f437546f425b2d09038a",
+    "url": "/my-app/static/css/main.e05ab3c9.chunk.css"
+  },
+  {
+    "revision": "e3482644ffaa074a68d8",
+    "url": "/my-app/static/js/2.da120bbd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/my-app/static/js/2.da120bbd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f437546f425b2d09038a",
+    "url": "/my-app/static/js/main.fc869466.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  },
+  {
+    "revision": "532ede27ecf0339f680c18dce4ada1d3",
+    "url": "/my-app/static/media/fotoPerfil1.532ede27.png"
+  }
+]);
